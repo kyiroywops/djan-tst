1523c0aa4d55b017483c045148d2a2e58bdf055e
Kylian Leroy <aleexisazul@gmail.com>
2022-09-08 19:41:02 -0300

    pages ruts

--- libreria/views.py
+++ libreria/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Create your views here.
+
 def inicio(request):
     return HttpResponse("<h1>hola mundo</h1>")
 
-# Create your views here.
+def nosotros(request):
+    return render(request, "paginas/nosotros.html")
