31c23acbf1663961234bdef52c3727fafdc3c826
Kylian Leroy <aleexisazul@gmail.com>
2022-09-19 21:32:47 -0300

    migrate

--- libreria/models.py
+++ libreria/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 class Libro(models.Model):
     id = models.AutoField(primary_key=True)
-    titulo = models.CharField(max_length=100)
+    titulo = models.CharField(max_length=100, verbose_name='Titulo')
     imagen = models.ImageField(upload_to="imagenes/", null=True, verbose_name='Imagen')
     descripcion = models.TextField(null=True, verbose_name='Descripción')
