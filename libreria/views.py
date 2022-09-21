2ffb0bdf7c0750316eefdd26eb096b329dab393a
Kylian Leroy <aleexisazul@gmail.com>
2022-09-21 13:06:07 -0300

    views y ed el

--- libreria/views.py
+++ libreria/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Libro
 from django.http import HttpResponse
+from .forms import LibroForm
 
 # Create your views here.
 
@@ -15,8 +16,18 @@ def libros(request):
     return render(request, "libros/index.html", {"libros": libros})
 
 def crear(request):
-    return render(request, "libros/crear.html")
+    formulario = LibroForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect("libros")
+    return render(request, "libros/crear.html", {"formulario": formulario})
 
-def editar(request):
-    return render(request, "libros/editar.html")
+def editar(request, id):
+    libro = Libro.objects.get(id=id)
+    formulario = LibroForm(request.POST or None, request.FILES or None, instance=libro)
+    return render(request, "libros/editar.html", {"formulario": formulario})
 
+def eliminar(request, id):
+    libro = Libro.objects.get(id=id)
+    libro.delete()
+    return redirect("libros")
